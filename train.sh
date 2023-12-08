fb6b76c611add894ab321c8b2f92f5e42aec7e6a
Gabriel Vidal-Ayrinhac <gvidalayrinhac@gmail.com>
2023-12-08 20:27:23 +0100

    tiling colors

--- train.sh
+++ train.sh
@@ -12,9 +12,10 @@ accelerate launch train_controlnet_sdxl.py \
  --learning_rate=1e-5 \
  --max_train_steps=15000 \
  --validation_image "./palette1.png" "./palette2.png" "./palette3.png"   \
- --validation_prompt "red circle with blue background" "cyan circle with brown floral background" \
+ --validation_prompt "tree house on the beach" "cat warrior" "screaming dogs illustration" \
  --validation_steps=50 \
- --train_batch_size=1 \
+ --pretrained_vae_model_name_or_path="madebyollin/sdxl-vae-fp16-fix" \
+ --train_batch_size=16 \
  --gradient_accumulation_steps=4 \
  --report_to="wandb" \
  --seed=42 
