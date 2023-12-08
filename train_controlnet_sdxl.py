fe3615d4ffda497378921dcca33445081d0e7710
Gabriel Vidal-Ayrinhac <gvidalayrinhac@gmail.com>
2023-12-08 18:00:49 +0100

    update conditionning image

--- train_controlnet_sdxl.py
+++ train_controlnet_sdxl.py
@@ -789,12 +789,6 @@ def prepare_train_dataset(dataset, accelerator):
 
     image_transforms = transforms.Compose(
         [
-            transforms.Resize(
-                args.resolution, interpolation=transforms.InterpolationMode.BILINEAR
-            ),
-            transforms.CenterCrop(args.resolution),
-            transforms.ColorJitter(hue=0.5),
-            transforms.RandomHorizontalFlip(p=0.5),
             transforms.ToTensor(),
             transforms.Normalize([0.5], [0.5]),
         ]
@@ -802,24 +796,17 @@ def prepare_train_dataset(dataset, accelerator):
 
     conditioning_image_transforms = transforms.Compose(
         [
-            transforms.Resize(
-                args.resolution, interpolation=transforms.InterpolationMode.BILINEAR
-            ),
-            transforms.CenterCrop(args.resolution),
+            Palettify(),
             transforms.ToTensor(),
         ]
     )
 
     def preprocess_train(examples):
         images = [image.convert("RGB") for image in examples[args.image_column]]
-        images = [image_transforms(image) for image in images]
+        images = [common_transforms(image) for image in images]
 
-        conditioning_images = [
-            image.convert("RGB") for image in examples[args.conditioning_image_column]
-        ]
-        conditioning_images = [
-            conditioning_image_transforms(image) for image in conditioning_images
-        ]
+        conditioning_images = [conditioning_image_transforms(image) for image in images]
+        images = [image_transforms(image) for image in images]
 
         examples["pixel_values"] = images
         examples["conditioning_pixel_values"] = conditioning_images
